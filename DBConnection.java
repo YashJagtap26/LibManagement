f3bacaf51e35e92a2e935bfd48b18fb03e152b2e
YashJagtap26 <147371031+YashJagtap26@users.noreply.github.com>
2025-01-31 15:22:57 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/library_db";
+    private static final String USER = "root"; // Change this if your MySQL username is different
+    private static final String PASSWORD = ""; // Change this if you have a MySQL password
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
